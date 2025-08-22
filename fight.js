7dc1ebbbab9a02ab9138c63a379814f90d7312da
Yurchenko Elena <echistova@gmail.com>
2025-08-22 15:37:15 +0300

    feat: character

---
+++ fight.js
@@ -0,0 +1,12 @@
+const caracters = [
+  { id: 1, src: "./assets/img/fenek.jpg", alt: "Fenek" },
+  { id: 2, src: "./assets/img/berkut.jpg", alt: "Berkut" },
+  { id: 3, src: "./assets/img/cat.jpg", alt: "Cat" },
+  { id: 4, src: "./assets/img/varan.jpg", alt: "Varan" },
+  { id: 5, src: "./assets/img/peregusna.jpg", alt: "Peregusna" },
+];
+const enemy = [
+  { id: 1, src: "./assets/img/сobra.jpg", alt: "Fenek" },
+  { id: 2, src: "./assets/img/viper.jpg", alt: "Berkut" },
+  { id: 3, src: "./assets/img/scorpion.jpg", alt: "Scorpion" },
+];
