14240819f809a8482e6405d70d7d9318bd283460
Yurchenko Elena <echistova@gmail.com>
2025-08-24 08:23:45 +0300

    feat: hero pages

--- person.js
+++ person.js
@@ -7,8 +7,8 @@ window.appData = {
         { id: 5, src: "./assets/img/peregusna.jpg", alt: "Peregusna" },
     ],
     enemy: [
-        { id: 1, src: "./assets/img/сobra.jpg", alt: "Fenek" },
-        { id: 2, src: "./assets/img/viper.jpg", alt: "Berkut" },
+        { id: 1, src: "./assets/img/cobra.jpg", alt: "Cobra" },
+        { id: 2, src: "./assets/img/viper.jpg", alt: "Viper" },
         { id: 3, src: "./assets/img/scorpion.jpg", alt: "Scorpion" },
     ],
 };
